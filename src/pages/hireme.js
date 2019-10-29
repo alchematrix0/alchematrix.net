fe4667398ff67ba8cfd50a78735dd3207949cbed
alchematrix0 <alchematrix0@gmail.com>
2019-10-29 13:44:22 -0700

    update hire me with new referral and some social buttons :)

--- src/pages/hireme.js
+++ src/pages/hireme.js
@@ -1,5 +1,7 @@
 import React from "react"
 import Layout from "../components/layout"
+import { FiLinkedin, FiGithub } from "react-icons/fi"
+import { FaStackOverflow, FaSpotify, FaAngellist } from "react-icons/fa"
 
 const HireMe = () => {
   const offerings = [
@@ -9,8 +11,8 @@ const HireMe = () => {
     {description: "Single Page App", timeline: "2 weeks", price: "$375", example: "Farmer Resource Database", link: "https://www.lillooetagricultureandfood.org/farmer-resource-database/" },
     {description: "Single Page App w/ Purchases", timeline: "4 weeks", price: "$600", example: "PoCake", link: "https://pocake.alchematrix.net"},
     {description: "Platform Economy App", timeline: "8 weeks", price: "$2,500", example: "Taski", link: "https://www.taskiapp.com"},
-    {description: "Education", timeline: "Hourly", price: "$100", example: "MD5", link: "https://desforets.github.io/md5/"},
-    {description: "Existing project (bugs, features, deployment, security)", timeline: "Hourly", price: "$100", example: "JTF Canada Migration", link: "https://jtfcanada.com"}
+    {description: "Education", timeline: "Hourly", price: "$80", example: "MD5", link: "https://desforets.github.io/md5/"},
+    {description: "Existing project (bugs, features, deployment, security)", timeline: "Hourly", price: "$60", example: "JTF Canada Website Migration", link: "https://jtfcanada.com"}
   ]
   const TableRow = (props) => (
     <tr>
@@ -21,27 +23,28 @@ const HireMe = () => {
     </tr>
   )
   const testimonials = [
-    {name: "Marianne", org: "LAFS", project: "Farmer Resource Database", date: "1/10/2018", comment: "Works fast and communicates well. Brought my vision to life accurately and on budget. Definitely goes the extra mile."},
-    {name: "Toby", org: "Lillooet Library", project: "Digital Literacy Workshop", date: "8/20/2016", comment: "Awesome and informative presentation, clearly delivered and surprisingly fun."},
-    {name: "Peter", org: "EarthSun Organics", project: "EarthSun Wholesale Website", date: "1/12/2018", comment: "Got the work done faster than we thought and provided ongoing support at a reasonable rate."},
-    {name: "Rishabh", org: "Taski", project: "Platform Economy Web App", date: "Ongoing", comment: "We could not find a project challenging enough for this guy. Throw anything at him, he'll figure it out!"}
+    { name: "Toby", org: "Lillooet Library", project: "Digital Literacy Workshop", date: "8/20/2016", comment: "Awesome and informative presentation, clearly delivered and surprisingly fun." },
+    { name: "Peter", org: "EarthSun", project: "Wholesale E-commerce", date: "1/12/2017", comment: "Got the work done faster than we thought and provided ongoing support at a reasonable rate." },
+    { name: "Marianne", org: "LAFS", project: "Farmer Resource Database", date: "1/6/2018", comment: "Works fast and communicates well. Brought my vision to life accurately and on budget. Definitely goes the extra mile." },
+    { name: "Rishabh", org: "Taski", project: "Platform Economy Web App", date: "Ongoing", comment: "We could not find a project challenging enough for this guy. Throw anything at him, he'll figure it out!" },
+    { name: "Seb", org: "Ora Organic", project: "Referral Program", date: "1/03/2019", comment: "Highly skilled, incredibly responsive, worked quickly, precisely, and very independently. A pleasure to work with; would recommend him for any role with the technical or non-technical side of the business. "}
   ]
-  const Testimonial = (props) => (
-    <div className="column is-one-quarter">
-      <div class="card" style={{maxWidth: "400px", display: "inline-block"}}>
+  const Testimonial = ({ testimonial }) => (
+    <div className={`column ${testimonial.name === "Seb" ? "is-full" : "is-half"}`}>
+      <div class="card" style={{display: "inline-block"}}>
         <div class="card-content">
-          <p class="title is-4">{props.data.comment}</p>
-          <p class="subtitle"><em>{props.data.name} <small>({props.data.org})</small></em></p>
+          <p class="title is-5">{testimonial.comment}</p>
+          <p class="subtitle"><em>{testimonial.name} <small>-- {testimonial.org}</small></em></p>
         </div>
         <footer class="card-footer">
           <p class="card-footer-item">
             <span>
-              {props.data.project}
+              {testimonial.project}
             </span>
           </p>
           <p class="card-footer-item">
             <span>
-              {props.data.date}
+              {testimonial.date}
             </span>
           </p>
         </footer>
@@ -68,7 +71,36 @@ const HireMe = () => {
       <section>
         <h1 className="title is-spaced" style={{marginTop: "2em"}}>Testimonials</h1>
         <div className="columns is-multiline">
-          {testimonials.map(t => (<Testimonial data={t} />))}
+          {testimonials.map(t => (<Testimonial testimonial={t} />))}
+        </div>
+      </section>
+      <section>
+        <div className="columns is-centered is-mobile" style={{marginTop: "4em"}}>
+          <div className="column is-1">
+            <a target="_blank" rel="noopener noreferrer" href="https://linkedin.com/in/alchematrix0" className="button is-light">
+              <span className="icon"><FiLinkedin /></span>
+            </a>
+          </div>
+          <div className="column is-1">
+            <a target="_blank" rel="noopener noreferrer" href="https://github.com/alchematrix0" className="button is-light">
+              <span className="icon"><FiGithub /></span>
+            </a>
+          </div>
+          <div className="column is-1">
+            <a target="_blank" rel="noopener noreferrer" href="https://stackoverflow.com/users/5691584/alphanumeric0101" className="button is-light">
+              <span className="icon"><FaStackOverflow /></span>
+            </a>
+          </div>
+          <div className="column is-1">
+            <a target="_blank" rel="noopener noreferrer" href="https://open.spotify.com/user/06xgxgcxwcj3fgtwdlx4tna1t" className="button is-light">
+              <span className="icon"><FaSpotify /></span>
+            </a>
+          </div>
+          <div className="column is-1">
+            <a target="_blank" rel="noopener noreferrer" href="https://angel.co/michel_alexander" className="button is-light">
+              <span className="icon"><FaAngellist /></span>
+            </a>
+          </div>
         </div>
       </section>
   	</Layout>
